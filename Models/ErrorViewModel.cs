9968234fe309f1fdc980027d241e525d7839f4b6
Joseph Iglecias <igleciasjoseph@gmail.com>
2019-08-19 20:05:55 -0400

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SaharaC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
